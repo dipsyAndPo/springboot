dec178a9c226bc7121e1babf873d934439758f7d
yyt <976678755@qq.com>
2018-11-07 11:31:02 +0800

    1

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ springboot框架练习
 
 这是一个简单的springboot框架，同时使用了mybatis与mysql，和pagehelper的插件
 
-   <h1>标记有详细的注释可以看
+   <h1>代码中有详细的注释可以看
+   
+<ul>
+    <li>分页增删改查</li>
+    <li>上传图片</li>
+    <li></li>
--- src/main/java/cn/yyt/springboot/web/HelloController.java
+++ src/main/java/cn/yyt/springboot/web/HelloController.java
@@ -13,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -63,8 +66,20 @@ public class HelloController {
 
     @RequestMapping(value = "upload",method = RequestMethod.POST)
     public String upload(HttpServletRequest req, @RequestParam("file")MultipartFile file,Model m){
-
+        try {
             String fileName=System.currentTimeMillis()+file.getOriginalFilename();
+            String destFileName=req.getServletContext().getRealPath("")+"upload"+ File.separator+fileName;
+            File destFile=new File(destFileName);
+            destFile.getParentFile().mkdirs();
+            file.transferTo(destFile);
+            m.addAttribute("fileName",fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return "上传失败," + e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "上传失败," + e.getMessage();
+        }
         return "showImg";
     }
 
