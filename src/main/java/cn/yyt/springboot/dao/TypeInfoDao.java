d8277e4867671edea81a246c475a3c92dbc81203
yyt <976678755@qq.com>
2018-12-10 19:47:32 +0800

    提交

--- src/main/java/cn/yyt/springboot/dao/TypeInfoDao.java
+++ src/main/java/cn/yyt/springboot/dao/TypeInfoDao.java
@@ -5,5 +5,6 @@ import cn.yyt.springboot.pojo.TypeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TypeInfoDao extends JpaRepository<TypeInfo,Integer> {
+    
 
 }
